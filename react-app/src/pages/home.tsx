6d88cb93f13e6255ded8f03b0c2248d49341cfd2
BrandNewLifeJackie26 <jackie020130923@126.com>
2021-06-09 00:07:49 -0700

    Add components and improve homepage UI

--- react-app/src/pages/home.tsx
+++ react-app/src/pages/home.tsx
@@ -1,12 +1,25 @@
 import styles from './home.css';
-import { Layout } from 'antd';
+import { Button } from 'antd';
+import { PieChartTwoTone, ProjectTwoTone } from '@ant-design/icons';
 
 export default function Home() {
     return (
-        <div className={styles.welcomeWord}>
-            Welcome to Your<br/>
-            1st Professional<br/>
-            Analytical Tool<br/>
+        <div className={styles.container}>
+            <div className={styles.welcomeWord}>
+                Welcome to The<br/>
+                No.1 Professional<br/>
+                Analytical Tool<br/>
+            </div>
+            <div className={styles.intro}>
+                <div className={styles.analysisIntro}>
+                    <PieChartTwoTone twoToneColor="#0084ff" className={styles.icon}></PieChartTwoTone>
+                    <Button type="primary" shape="round" className={styles.buttonLearnMore}>Learn More</Button>
+                </div>
+                <div className={styles.recommendationIntro}>
+                    <ProjectTwoTone twoToneColor="#188fff" className={styles.icon}></ProjectTwoTone>
+                    <Button type="primary" shape="round" className={styles.buttonLearnMore}>Learn More</Button>
+                </div>
+            </div>
         </div>
     );
 }
