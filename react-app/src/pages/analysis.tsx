9009f12906df61ae54816479d5bbc9c0f45f97c1
BrandNewLifeJackie26 <jackie020130923@126.com>
2021-06-09 17:15:45 -0700

    Add UI to analysis page.

--- react-app/src/pages/analysis.tsx
+++ react-app/src/pages/analysis.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Carousel } from 'antd';
+
 import styles from './analysis.css';
-import { useState } from 'react';
 import BarChart from '../components/charts/barchart'
 
 export default function Analysis() {
@@ -73,59 +74,86 @@ export default function Analysis() {
     }, []);
   
     return (
-      <>
-        <div>
-        <h1 className={styles.title}>Analysis</h1>
-        </div>
+      // <>
+      //   <div>
+      //   <h1 className={styles.title}>Analysis</h1>
+      //   </div>
 
-        <div>
-        <h2 className={styles.subtitle}>Category Level</h2>
-        </div>
-        <table width='100%'>
-          <tbody>
-            <tr>
-              <td align='center'><BarChart spec={specTopCategoriesBySales}></BarChart></td>
-              <td align='center'><BarChart spec={specCustomerBehaviorByCategory}></BarChart></td>
-            </tr>
-            <tr>
-              <td align='center'><BarChart spec={specTopBrandsByCategory}></BarChart></td>
-              <td align='center'><BarChart spec={specTopItemsByCategory}></BarChart></td>
-            </tr>
-          </tbody>
-        </table>
+      //   <div>
+      //   <h2 className={styles.subtitle}>Category Level</h2>
+      //   </div>
+      //   <table width='100%'>
+      //     <tbody>
+      //       <tr>
+      //         <td align='center'><BarChart spec={specTopCategoriesBySales}></BarChart></td>
+      //         <td align='center'><BarChart spec={specCustomerBehaviorByCategory}></BarChart></td>
+      //       </tr>
+      //       <tr>
+      //         <td align='center'><BarChart spec={specTopBrandsByCategory}></BarChart></td>
+      //         <td align='center'><BarChart spec={specTopItemsByCategory}></BarChart></td>
+      //       </tr>
+      //     </tbody>
+      //   </table>
         
-        <div>
-        <h2 className={styles.subtitle}>Brand Level</h2>
-        </div>
-        <table width='100%'>
-          <tbody>
-            <tr>
-              <td align='center'><BarChart spec={specTopBrandsBySales}></BarChart></td>
-              <td align='center'><BarChart spec={specCustomerBehaviorByBrand}></BarChart></td>
-            </tr>
-            <tr>
-              <td align='center'><BarChart spec={specTopItemsByBrand}></BarChart></td>
-            </tr>
-          </tbody>
-        </table>
+      //   <div>
+      //   <h2 className={styles.subtitle}>Brand Level</h2>
+      //   </div>
+      //   <table width='100%'>
+      //     <tbody>
+      //       <tr>
+      //         <td align='center'><BarChart spec={specTopBrandsBySales}></BarChart></td>
+      //         <td align='center'><BarChart spec={specCustomerBehaviorByBrand}></BarChart></td>
+      //       </tr>
+      //       <tr>
+      //         <td align='center'><BarChart spec={specTopItemsByBrand}></BarChart></td>
+      //       </tr>
+      //     </tbody>
+      //   </table>
 
-        <div>
-        <h2 className={styles.subtitle}>Daily Level</h2>
-        </div>
-        <table width='100%'>
-          <tbody>
-            <td align='center'><BarChart spec={specDailySalesByCategoryAndBrand}></BarChart></td>
-          </tbody>
-        </table>
+      //   <div>
+      //   <h2 className={styles.subtitle}>Daily Level</h2>
+      //   </div>
+      //   <table width='100%'>
+      //     <tbody>
+      //       <td align='center'><BarChart spec={specDailySalesByCategoryAndBrand}></BarChart></td>
+      //     </tbody>
+      //   </table>
 
-        <div>
-        <h2 className={styles.subtitle}>Others</h2>
+      //   <div>
+      //   <h2 className={styles.subtitle}>Others</h2>
+      //   </div>
+      //   <table width='100%'>
+      //     <tbody>
+      //       <td align='center'><BarChart spec={specCustomerBehaviorByCategoryAndBrand}></BarChart></td>
+      //     </tbody>
+      //   </table>
+      // </>
+
+      <>
+        <div className={styles.levelSelection}>
+          <div className={styles.selectionIntro}>Click Level to Analyse</div>
+          <div className={styles.selections}>
+            <Button type="primary" shape="round" className={styles.selectionButton}>Category</Button>
+            <Button type="primary" shape="round" className={styles.selectionButton}>Brand</Button>
+            <Button type="primary" shape="round" className={styles.selectionButton}>Daily</Button>
+            <Button type="primary" shape="round" className={styles.selectionButton}>Others</Button>
+          </div>
         </div>
-        <table width='100%'>
-          <tbody>
-            <td align='center'><BarChart spec={specCustomerBehaviorByCategoryAndBrand}></BarChart></td>
-          </tbody>
-        </table>
+
+        <div className={styles.analysis}>
+          <div className={styles.analysisIntro}>
+            <div className={styles.analysisIntroTitle}>Category</div>
+            <div className={styles.analysisIntroBody}>Category Body</div>
+          </div>
+          <div className={styles.analysisChart}>
+            <Carousel className={styles.carousel}>
+              <div className={styles.carouselItem}>1</div>
+              <div className={styles.carouselItem}>2</div>
+              <div className={styles.carouselItem}>3</div>
+              <div className={styles.carouselItem}>4</div>
+            </Carousel>
+          </div>
+        </div>
       </>
     );
   }
